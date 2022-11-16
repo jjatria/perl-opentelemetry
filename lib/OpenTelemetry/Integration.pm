4807fb5cbe31c72de0f78690b7c9da742a3f4260
José Joaquín Atria <jjatria@gmail.com>
2022-11-16 12:34:38 +0000

    Port ingegration model from OpenTracing

---
+++ lib/OpenTelemetry/Integration.pm
@@ -0,0 +1,111 @@
+package OpenTelemetry::Integration;
+# ABSTRACT: Top-level interface for OpenTelemetry integrations
+
+our $VERSION = '0.001';
+
+use experimental qw( try signatures );
+
+use Syntax::Keyword::Try;
+use Module::Pluggable;
+use Module::Load ();
+use List::Util 'uniqstr';
+
+use Log::Any '$log';
+
+sub import ( $class, @args ) {
+    return unless @args;
+
+    # If we have an explicit list, then we load
+    # dependencies - this is turned off by :all tag
+    my $load_deps = 1;
+
+    my @modules;
+    for my $target (@args) {
+        # Try to load *all* available integrations
+        if ( $target eq ':all' ) {
+            push @modules, $class->plugins;
+            $load_deps = 0;
+            last;
+        }
+
+        push @modules, __PACKAGE__ . '::' . $target;
+    }
+
+    for my $module ( uniqstr @modules ) {
+        try {
+            $log->tracef('Loading %s', $module);
+            Module::Load::load($module);
+            $module->load($load_deps);
+        } catch ($e) {
+            # Just a warning, if we're loading everything then
+            # we shouldn't cause chaos just because something
+            # doesn't happen to be available.
+            $log->warnf('Unable to loading OpenTelemetry integration %s: %s', $module, $e);
+        }
+    }
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+OpenTelemetry::Integration - Top-level interface for OpenTelemetry integrations
+
+=head1 SYNOPSIS
+
+    # Load integrations specific modules
+    use OpenTelemetry::Integration qw( HTTP::Tiny DBI );
+
+    # Load  every available integration
+    use OpenTelemetry::Integration ':all';
+
+=head1 DESCRIPTION
+
+This is the base class for handling tracing integration with other CPAN
+modules.
+
+It provides functionality for loading available integrations
+via the import list on a C<use> statement:
+
+=over 4
+
+=item *
+
+with C<:all>, all available OpenTelemetry integrations will be loaded,
+if the module the integration is for has already been loaded
+
+=item *
+
+with a specific list of modules, they will be loaded (even if they haven't
+been before) and integrations for them will be applied if available
+
+=back
+
+This means that you can expect L<HTTP::Tiny> to be traced if you have the
+L<OpenTelemetry::Integration::HTTP::Tiny> module installed and you do this:
+
+    use OpenTracing::Integration 'HTTP::Tiny';
+
+or this:
+
+    use HTTP::Tiny;
+    use OpenTracing::Integration ':all';
+
+but it will B<not> be traced if you do this:
+
+    use OpenTracing::Integration ':all';
+    use HTTP::Tiny;
+
+The rationale behind this apparently inconsistent behaviour is that, with a
+large install, C<:all> might load unexpected integrations. This behaviour
+allows you instead to add this line after your module imports, and any
+functionality that is actively being used in the code (for which an
+integration module is available) would gain tracing.
+
+=head1 COPYRIGHT
+
+...
---
+++ lib/OpenTelemetry/Integration/HTTP/Tiny.pm
@@ -0,0 +1,96 @@
+package OpenTelemetry::Integration::HTTP::Tiny;
+# ABSTRACT: OpenTelemetry integration for HTTP::Tiny
+
+our $VERSION = '0.001';
+
+use strict;
+use warnings;
+use experimental 'signatures';
+
+use Class::Method::Modifiers 'install_modifier';
+use OpenTelemetry;
+
+use parent 'OpenTracing::Integration';
+
+my $loaded;
+sub load ( $class, $load_deps = 0 ) {
+    return if $loaded;
+    return unless $load_deps or 'HTTP::Tiny'->can('new');
+
+    require HTTP::Tiny;
+    require URI;
+
+    install_modifier HTTP::Tiny => around => request => sub {
+        my ( $code, $self, $method, $url, $options ) = @_;
+        my $uri = URI->new("$url");
+        my $path = $uri->path || '/';
+
+        # TODO: A high-level DSL? Links?
+        my $tracer = OpenTelemetry->tracer_provider->tracer( name => __PACKAGE__ );
+
+        my $span = $tracer->create_span(
+            name       => $path,,
+            kind       => 'client',
+            attributes => {
+                'http.method' => $method,
+                'http.url'    => "$url",
+                'http.user_agent' => $self->agent,
+                'http.flavor' => '1.1', # HTTP::Tiny always uses HTTP/1.1
+
+                # TODO: Should this be using ->host_port?
+                # As per https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/http.md#http-client
+                # When request target is absolute URI, net.peer.name MUST
+                # match URI port identifier, otherwise it MUST match Host
+                # header port identifier.
+                'net.peer.name' => $uri->host,
+                'net.peer.port' => $uri->port,
+
+                # TODO: http.request_content_length will require us
+                # to hook into write_request most likely
+            },
+        );
+
+        my $res = $self->$code( $method, $url, $options );
+        $span->set_attribute( 'http.status_code' => $res->{status} );
+
+        # TODO: this should include retries
+        $span->set_attribute( 'http.resend_count' => scalar @{ $res->{redirects} } )
+            if $res->{redirects};
+
+        unless ( $res->{success} ) {
+            my $description = $res->{status} == 599 ? ( $res->{content} // '' ) : '';
+            $span->set_status( error => $description );
+        }
+
+        my $length = $res->{headers}{'content-length'};
+        $span->set_attribute( 'http.response_content_length' => $length ) if defined $length;
+
+        return $res;
+    }
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+OpenTracing::Integration::HTTP::Tiny - OpenTelemetry integration for HTTP::Tiny
+
+=head1 SYNOPSIS
+
+    use OpenTelemetry::Integration 'HTTP::Tiny';
+    HTTP::Tiny->new->get('https://metacpan.org');
+
+=head1 DESCRIPTION
+
+See L<OpenTelemetry::Integration> for more details.
+
+Since this is a core module, it's included in the L<OpenTelemetry> core
+distribution as well.
+
+=head1 COPYRIGHT
+
+...
