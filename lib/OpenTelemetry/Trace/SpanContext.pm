85909625248f35254e565bc2335491c4730f9bca
José Joaquín Atria <jjatria@gmail.com>
2022-11-17 16:53:07 +0000

    Use TraceState and TraceFlags in SpanContext

--- lib/OpenTelemetry/Trace/SpanContext.pm
+++ lib/OpenTelemetry/Trace/SpanContext.pm
@@ -6,17 +6,20 @@ package OpenTelemetry::Trace::SpanContext;
 our $VERSION = '0.001';
 
 use OpenTelemetry::Trace::Common;
+    use OpenTelemetry::Propagator::TraceContext::TraceFlags;
+    use OpenTelemetry::Propagator::TraceContext::TraceState;
 
 class OpenTelemetry::Trace::SpanContext {
+
     has $trace_flags :param :reader = undef;
     has $trace_state :param :reader = undef;
     has $trace_id    :param :reader = undef;
     has $span_id     :param :reader = undef;
     has $remote      :param :reader = 0;
 
-    ADJUST {
-        $trace_flags //= 0;  # TODO: default flags
-        $trace_state //= {}; # TODO: default state
+    BUILD {
+        $trace_flags //= OpenTelemetry::Propagator::TraceContext::TraceFlags->new;
+        $trace_state //= OpenTelemetry::Propagator::TraceContext::TraceState->new;
         $trace_id    //= OpenTelemetry::Trace::Common->generate_trace_id;
         $span_id     //= OpenTelemetry::Trace::Common->generate_span_id;
     }
