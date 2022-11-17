b449f53259f6776018ddc6775247604b5d607d24
José Joaquín Atria <jjatria@gmail.com>
2022-11-17 20:07:57 +0000

    Add span status class

---
+++ lib/OpenTelemetry/Trace/Span/Status.pm
@@ -0,0 +1,20 @@
+use Object::Pad;
+# ABSTRACT: The status of an OpenTelemetry span
+
+package
+    OpenTelemetry::Trace::Span::Status;
+
+our $VERSION = '0.001';
+
+class OpenTelemetry::Trace::Span::Status {
+    has $code        :param         = 'UNSET';
+    has $description :param :reader = '';
+
+    ADJUST {
+        $code = 'UNSET' unless ( $code // '' ) =~ /^ (:? 'OK | 'ERROR' ) $/x;
+    }
+
+    method ok    { $code eq 'OK'    }
+    method error { $code eq 'ERROR' }
+    method unset { $code eq 'UNSET' }
+}
