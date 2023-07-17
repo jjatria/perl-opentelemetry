98bba02c52e34a4ac44c68303e095e7d9373b612
José Joaquín Atria <jjatria@gmail.com>
2023-07-17 21:41:35 +0100

    Remove unused logger in TracerProvider

--- lib/OpenTelemetry/Trace/TracerProvider.pm
+++ lib/OpenTelemetry/Trace/TracerProvider.pm
@@ -5,9 +5,6 @@ package OpenTelemetry::Trace::TracerProvider;
 
 our $VERSION = '0.001';
 
-use Log::Any;
-my $logger = Log::Any->get_logger( category => 'OpenTelemetry' );
-
 class OpenTelemetry::Trace::TracerProvider {
     use OpenTelemetry::Trace::Tracer;
 
