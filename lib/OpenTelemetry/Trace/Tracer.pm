9eed832380fd3b8c77dd24156bec41f6e73e4e6f
José Joaquín Atria <jjatria@gmail.com>
2023-07-17 21:40:36 +0100

    Make small tweaks to Tracer documentation

--- lib/OpenTelemetry/Trace/Tracer.pm
+++ lib/OpenTelemetry/Trace/Tracer.pm
@@ -77,7 +77,7 @@ A tracer is responsible for creating L<OpenTelemetry::Trace::Span> objects.
         start      => $timestamp  // time,
     )
 
-Creates a L<OpenTelemetry::Trace::Span> instance associated with this trace.
+Creates an L<OpenTelemetry::Trace::Span> instance associated with this trace.
 
 Takes a list of key / value pairs. Of these, the only one that callers are
 I<always> expected to provide is the span name: not doing so may result in
@@ -87,16 +87,17 @@ keys are optional and can be used to further specify the span.
 Note that even though the majority of these can be set after a span's
 creation, it's always recommended to set these on creation if possible, since
 this is the only time when they are guaranteed to be available for any
-behaviours that may depend on them (eg. san sampling).
+behaviours that may depend on them (eg. span sampling).
 
 Spans can have zero or more child spans, which represent causally related
-operations. These can be created via the C<parent> parameter, if it receives
-a L<OpenTelemetry::Context> that holds a span. If no parent is set, or if the
-provided context does not contain a span, the created span is considered a
-I<root span>. Typically, a trace will only have one root span.
+operations. These can be created by passing a
+L<OpenTelemetry::Context> that holds the parent span as the value for the
+C<parent> parameter. If no parent is set, or if the provided context does not
+contain a span, the created span is considered a I<root span>. Typically, a
+trace will only have one root span.
 
 Spans can also have links to other spans, including those that belong to a
-different trace. This can be set on span creation via the C<links> parameter,
+different trace. These can be set on span creation via the C<links> parameter,
 which must hold a reference to an array of hashes, each of which should
 be usable (when dereferenced) as the parameters to the span's
 L<add_link|OpenTelemetry::Trace::Span/add_link> method.
