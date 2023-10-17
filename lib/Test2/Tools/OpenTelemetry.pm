bd372a716aef92eac770ed1bf9cac45a40493405
José Joaquín Atria <jjatria@cpan.org>
2023-10-17 23:57:58 +0100

    Add experimental support for testing metrics

--- lib/Test2/Tools/OpenTelemetry.pm
+++ lib/Test2/Tools/OpenTelemetry.pm
@@ -4,10 +4,19 @@ package
 our $VERSION = '0.001';
 
 use Exporter 'import';
-our @EXPORT = qw( messages no_messages );
+our @EXPORT = qw(
+    messages
+    no_messages
+    metrics
+    no_metrics
+);
 
+use Feature::Compat::Defer;
 use Test2::API 'context';
 use Test2::Compare qw( compare strict_convert );
+
+# TODO: Cannot lexically set a metrics adapter
+use Metrics::Any::Adapter 'Test';
 
 require Log::Any::Adapter;
 
@@ -32,6 +41,37 @@ sub no_messages (&) {
     my $messages = shift->$capture_messages;
     my $context  = context;
     my $delta    = compare $messages, [], \&strict_convert;
+
+    if ( $delta ) {
+        $context->fail( $name, $delta->diag );
+    }
+    else {
+        $context->ok( 1, $name );
+    }
+
+    $context->release;
+    return !$delta;
+}
+
+my $capture_metrics = sub {
+    my $code = shift;
+
+    Metrics::Any::Adapter::Test->clear;
+    defer { Metrics::Any::Adapter::Test->clear }
+
+    $code->();
+
+    [ split /\n/, Metrics::Any::Adapter::Test->metrics ];
+};
+
+sub metrics (&) { goto $capture_metrics }
+
+sub no_metrics (&) {
+    my $name = 'No metrics collected';
+
+    my $data    = shift->$capture_metrics;
+    my $context = context;
+    my $delta   = compare $data, [], \&strict_convert;
 
     if ( $delta ) {
         $context->fail( $name, $delta->diag );
